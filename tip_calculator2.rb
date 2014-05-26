4d53bd778428abd01ae3c2682e51a776dfb88cd9
danid123 <danid123@users.noreply.github.com>
2014-05-26 15:30:34 -0700

    Updating calculator 1 and adding calculator 2.

--- tip_calculator2.rb
+++ tip_calculator2.rb
@@ -1,13 +1,19 @@
-#set variables
-meal_cost = 20.to_f
-tax_percent = 9.to_f
-tip_percent = 15.to_f
+puts "Please enter the price of your meal: "
+meal_cost = gets.to_f
+
+puts "Please enter the tax rate: "
+tax_percent = gets.to_f
+
+puts "What percentage tip do you want to leave: "
+tip_percent = gets.to_f
+
 
 #calculate values
 tax_value = meal_cost*tax_percent/100
 meal_subtotal = meal_cost+tax_value
 tip_value = meal_cost*tip_percent/100 #tip should be calculated on the pre-tax value
 meal_total = meal_subtotal+tip_value
+
 
 print "The pre-tax cost of your meal was $%.2f.\n" % meal_cost
 print "At %d%%, tax for this meal is $%.2f.\n" % [tax_percent, tax_value]
